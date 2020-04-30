8aa61bd0405b8a2405500dcda3c98e8384570d61
Alex Oliveira <alex.borgesoliveira@gmail.com>
2020-04-30 10:39:06 -0300

    Adicionando a linha que indica a primeira ação do treinamento , Pull Request
    Indicando no Readme a realização do treinamento sobre ISSUES
    
    close #1
    
    Criado o arquivo Operacoes_matematica_basicas.py com exemplos de operaçòes
    basicas possiveis em Python

---
+++ Operacoes_matematica_basicas.py
@@ -0,0 +1,24 @@
+soma = (10+2)
+print('O resultado da Soma entre 10 e 2 e:')
+print (soma)
+
+subtracao = (10-2)
+print('O resultado da subtração entre 10 e 2 é:')
+print(subtracao)
+
+divisao_real = (10/2)
+print('O resultado da divisão real entre 10 e 2 é :')
+print(divisao_real)
+
+divisao_inteira = (10//2)
+print('O resultado da divisão inteira entre 10 e 2 é:')
+print(divisao_inteira)
+
+resto_divisao = (10%2)
+print('O resto da divisão entre 10 e 2 é :')
+print(resto_divisao)
+
+exponenciacao = (10**2)
+print('O resultado da exponenciacao de 10 elevado a 2 é:')
+print(exponenciacao)
+
--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Repositor de exercícios  e exemplos em Python
 2.0 - Treinamdo processo de ISSUES
 
 3.0 - Criando arquivo Gitignor global
+
+4.0 - Tipos Inteiros - Criado o arquivo Operações_matemaicas_basica
+    com exemplos de operações basicas feitas em Pythom sendo usado 
+    o numeor 10 e 2 para os exempos:
+    
