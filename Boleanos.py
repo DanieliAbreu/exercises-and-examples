842ade01728801d7f2d0b56f54516c8922065ab0
Alex Oliveira <alex.borgesoliveira@gmail.com>
2020-04-30 21:35:42 -0300

    Adicionando a linha que indica a primeira ação do treinamento , Pull Request
    Indicando no Readme a realização do treinamento sobre ISSUES
    
    close #1
    
    Criado o arquivo Operacoes_matematica_basicas.py com exemplos de operaçòes
    basicas possiveis em Python
    
    Criado o arquivo float.py com exemplos de numeros flutuantes e a sintax
    do numero complexo, sendo feito a conversão de um numero flutuante para
    um numero inteiro e de um numero interio para um flutuante.
    
    Criado o arquivo Variavel com exemplos de variaveis permitidas no Python,
    exemplos de como fazer para atribuir valores as variaveis e como identificar
    o tipo de valor armazenado na variavel.
    
    Criando o arquivo Boleanos com exemplos de possibilidades de se utilizar os booleanso em python.

---
+++ Boleanos.py
@@ -0,0 +1,75 @@
+#Constantes da algebra booleana
+#True
+#False
+
+#Operações basica da algebra de booleam são:
+# 1 - not
+print('Primeira operação not')
+print('not True:')
+print(not True)
+print()
+
+print('not False:')
+print(not False)
+print()
+
+# 2 - or
+print('Segunda operação or')
+print('True or True :')
+print(True or True)
+print()
+
+print('True or False')
+print(True or False)
+print()
+
+print('False or True')
+print(False or True)
+print()
+
+print('False or False')
+print(False or False)
+print()
+
+#3 - and
+print('Terciera operação and')
+print('False and False')
+print(False and False)
+print()
+
+print('False and True')
+print(False and True)
+print()
+
+print('True and False')
+print(True and False)
+print()
+
+print('''True and True
+ Unica operação com o and que retorna
+ verdadeiro : ''')
+print(True and True)
+print()
+
+
+
+#Exemplos e sintax do tipo booleanos
+_valor1 = 10
+_valor2 = 9
+
+print('Valor 1 :' )
+print(_valor1)
+print('Valor 2 :' )
+print(_valor2)
+print()
+
+#RExemplos de respostas com valores booleanos
+print('Valor 1 é maior ou igual a valor 2 ?')
+print(_valor1 >= _valor2 )
+print()
+print('Valor 1 é <= Valor 2 ?')
+print(_valor1 <= _valor2)
+
+#
+
+
